09f488849e6dbbfec8b2cd29fc0b5e3fbfb2fcab
Павел <kombojiec@yandex.ru>
2020-12-17 16:30:21 +0300

    save after revers & fix name api.js

--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -37,9 +37,9 @@ const EditProfilePopup = (props) =>{
       onOutsideClose={props.onOutsideClose} 
       title={'Редактировать профиль'} name='form_profile' 
       buttonName='Сохранить' onSubmit={handleSubmit} >
-      <input type="text" className="popup__input popup__input_place_name" name="name" id="edit-name" minLength="2" maxLength="40" value={name} onChange={handleNameChange}/>
+      <input type="text" className="popup__input popup__input_place_name" name="name" id="edit-name" minLength="2" maxLength="40" value={name || ''} onChange={handleNameChange}/>
       <label className="popup__input-error" htmlFor="edit-name" id="edit-name-error"></label>
-      <input type="text" className="popup__input popup__input_place_business" name="about" id="edit-business" minLength="2" maxLength="200" required value={description} onChange={handleDescriptionChange} />
+      <input type="text" className="popup__input popup__input_place_business" name="about" id="edit-business" minLength="2" maxLength="200" required value={description || ''} onChange={handleDescriptionChange} />
       <label className="popup__input-error" htmlFor="edit-business" id="edit-business-error"></label>
     </PopupWithForm>
   )
