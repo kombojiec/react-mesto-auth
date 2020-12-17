e59c7434802f47139f4b649134ecd611c945b991
Павел <kombojiec@yandex.ru>
2020-12-17 18:40:21 +0300

    commit before rolling back

--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -46,18 +46,13 @@ const EditProfilePopup = (props) =>{
       onOutsideClose={props.onOutsideClose} 
       title={'Редактировать профиль'} name='form_profile' 
       buttonName='Сохранить' onSubmit={handleSubmit} >
-<<<<<<< HEAD
-      <input type="text" className="popup__input popup__input_place_name" name="name" id="edit-name" minLength="2" maxLength="40" required ref={inputName} onChange={handleNameChange}/>
-      <label className="popup__input-error" htmlFor="edit-name" id="edit-name-error"></label>
-      <input type="text" className="popup__input popup__input_place_business" name="about" id="edit-business" minLength="2" maxLength="200" required ref={inputDescription} onChange={handleDescriptionChange} />
-=======
       <input type="text" className="popup__input popup__input_place_name" name="name" id="edit-name" minLength="2" maxLength="40" value={name || ""} onChange={handleNameChange}/>
       <label className="popup__input-error" htmlFor="edit-name" id="edit-name-error"></label>
       <input type="text" className="popup__input popup__input_place_business" name="about" id="edit-business" minLength="2" maxLength="200" required value={description  || ""} onChange={handleDescriptionChange} />
->>>>>>> develop
       <label className="popup__input-error" htmlFor="edit-business" id="edit-business-error"></label>
     </PopupWithForm>
   )
 
 }
+
 export default EditProfilePopup;
